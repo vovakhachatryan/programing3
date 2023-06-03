85547bd202d2ecfe0042f0ae44aeef6445832549
vovakhachatryan <vova.khachatryan@tumo.org>
2023-06-03 15:49:13 +0400

    new main class

--- classgishaticheater.js
+++ classgishaticheater.js
@@ -1,12 +1,8 @@
-class GishatichEater {
+class GishatichEater extends LivingCreature{
     constructor(x, y, index) {
-        this.x = x;
-        this.y = y;
+        super(x, y, index);
         this.energy = 8;
-        this.index = index;
-        this.directcions = [];
     }
-
     getNewCoordinates() {
         this.directions = [
             [this.x - 1, this.y - 1],
@@ -24,20 +20,9 @@ class GishatichEater {
 
     chooseCell(character) {
         this.getNewCoordinates()
-        var found = [];
-        for (var i in this.directions) {
-            var x = this.directions[i][0];
-            var y = this.directions[i][1];
-            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
-                if (matrix[y][x] == character) {
-                    found.push(this.directions[i]);
-                }
-            }
-        }
-        return found;
-
-
+       return super.chooseCell(character)
     }
+
 
     mul() {
         var emptyCells = this.chooseCell(0);
--- classgrasseater.js
+++ classgrasseater.js
@@ -1,10 +1,7 @@
-class GrassEater {
+class GrassEater extends LivingCreature {
     constructor(x, y, index) {
-        this.x = x;
-        this.y = y;
+       super(x,y,index);
         this.energy = 8;
-        this.index = index;
-        this.directcions = [];
     }
 
     getNewCoordinates() {
@@ -18,25 +15,11 @@ class GrassEater {
             [this.x, this.y + 1],
             [this.x + 1, this.y + 1]
         ];
-
     }
-
 
     chooseCell(character) {
         this.getNewCoordinates()
-        var found = [];
-        for (var i in this.directions) {
-            var x = this.directions[i][0];
-            var y = this.directions[i][1];
-            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
-                if (matrix[y][x] == character) {
-                    found.push(this.directions[i]);
-                }
-            }
-        }
-        return found;
-
-
+       return super.chooseCell(character)
     }
 
     mul() {
